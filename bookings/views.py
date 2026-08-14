677cdf5a8d62ebce954635b1da8501dc06a3efa1
Miss Terry Davis <tdavis_909@hotmail.com>
2026-08-14 18:27:48 +0100

    Add templates services page and start booking form

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import GroomingService
+from .forms import AppointmentForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def services(request):
+    services = GroomingService.objects.all()
+    return render(request, 'services.html', {'services': services})
+
+def book_appointment(request):
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment = form.save(commit=False)
+            appointment.user = request.user
+            appointment.save()
+            return redirect('home')
+    else:
+        form = AppointmentForm()
+    return render(request, 'book_appointment.html', {'form': form})
