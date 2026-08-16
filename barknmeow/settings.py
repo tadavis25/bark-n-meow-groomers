58e1886705ab5f11a78f9d6fa6f4179dc5537e4a
Miss Terry Davis <tdavis_909@hotmail.com>
2026-08-16 17:01:58 +0100

    Prepare project fordeployment

--- barknmeow/settings.py
+++ barknmeow/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.isfile('env.py'):
+    import env
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
