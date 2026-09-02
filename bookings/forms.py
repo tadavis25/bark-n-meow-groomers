d0db903e409aae97394ed43b501b0b5fe14cf727
Miss Terry Davis <tdavis_909@hotmail.com>
2026-09-02 13:43:49 +0100

    Add style to book appointment page

--- bookings/forms.py
+++ bookings/forms.py
@@ -3,16 +3,32 @@ from .models import Appointment, Pet
 from django.utils import timezone
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
+
+
 class AppointmentForm(forms.ModelForm):
     def __init__(self, *args, user=None, **kwargs):
         super().__init__(*args, **kwargs)
         if user:
             self.fields['pet'].queryset = Pet.objects.filter(owner=user)
 
+        self.fields['appointment_date'].widget = DateInput()
+        self.fields['appointment_time'].widget = TimeInput(
+              attrs={'step': 900})
+
     class Meta:
         model = Appointment
         fields = ['pet', 'service', 'appointment_date', 
                   'appointment_time', 'notes', ]
+
+    widgets = {
+        'appointment_date': DateInput(),
+    }
 
     def clean_appointment_date(self):
         appointment_date = self.cleaned_data["appointment_date"]
