ab5619bc47dbc2e9b3e95de7268f197358e8bae0
Miss Terry Davis <tdavis_909@hotmail.com>
2026-09-21 17:48:26 +0100

    Use secure Cloudinary image URLs

--- bookings/models.py
+++ bookings/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+import cloudinary
+
+cloudinary.config(secure=True)
 
 
 class Pet(models.Model):
